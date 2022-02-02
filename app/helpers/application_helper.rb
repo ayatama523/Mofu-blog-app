3a8d8546cf2f67677ccecdce16f835fb782f725c
ryunakae <ryunakae@gmail.com>
2022-02-02 23:05:29 +0900

    add sub category to category

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def category_options(categories)
+    return if categories.blank?
+
+    categories.map do |category|
+      [category.name, category.id]
+    end
+  end
 end
