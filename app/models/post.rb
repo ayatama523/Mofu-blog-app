5e7176b36e11618a904a51aff70b7f973cdd08ba
ryunakae <ryunakae@gmail.com>
2022-02-03 10:56:48 +0900

    fix code layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,6 +24,6 @@
 #  fk_rails_...  (creator_id => users.id)
 #
 class Post < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  belongs_to :creator, class_name: 'User'
   belongs_to :category, optional: true
 end
